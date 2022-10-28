f9a72811f5ef39b12f7cc96a02896afdf9cb7c54
Sandesh <sandeshj@bsf.io>
2022-10-28 17:04:51 +0530

    Fixed PHPCBF

--- tests/unit/SettingsTest.php
+++ tests/unit/SettingsTest.php
@@ -1,28 +1,27 @@
 <?php
 
 class SettingsTest extends \WP_UnitTestCase {
-    public $client;
+	public $client;
 
-    public function setUp() {
+	public function setUp() {
 		parent::setUp();
-        $this->client = new \SureCart\Licensing\Client( 'SureCart', __FILE__ );
-    }
+		$this->client = new \SureCart\Licensing\Client( 'SureCart', __FILE__ );
+	}
 
-    /**
-     * @dataProvider propertyProvider
-     */
-    public function test_can_get_and_set_options( $property, $value ) {
-        $this->assertEmpty($this->client->settings()->get_options());
-        $this->client->settings()->$property = $value;
-        $this->assertSame($value, $this->client->settings()->get_options()['sc_' . $property] );
-    }
+	/**
+	 * @dataProvider propertyProvider
+	 */
+	public function test_can_get_and_set_options( $property, $value ) {
+		$this->assertEmpty( $this->client->settings()->get_options() );
+		$this->client->settings()->$property = $value;
+		$this->assertSame( $value, $this->client->settings()->get_options()[ 'sc_' . $property ] );
+	}
 
-    public function propertyProvider(): array
-    {
-        return [
-            'license id' => ['license_id', 'test_id'],
-            'license key' => ['license_key', 'test_key'],
-            'activation id' => ['activation_id', 'test_activation_id'],
-        ];
-    }
+	public function propertyProvider(): array {
+		return array(
+			'license id'    => array( 'license_id', 'test_id' ),
+			'license key'   => array( 'license_key', 'test_key' ),
+			'activation id' => array( 'activation_id', 'test_activation_id' ),
+		);
+	}
 }
