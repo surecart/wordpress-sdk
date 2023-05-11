3e389a3a24745c5b2c733b63917cfd73c5ef9a5d
maniruzzaman.akash <manirujjamanakash@gmail.com>
2023-05-11 17:57:27 +0600

    test: Test cases to see hard refresh

--- tests/unit/SettingsTest.php
+++ tests/unit/SettingsTest.php
@@ -1,6 +1,11 @@
 <?php
 
 class SettingsTest extends \WP_UnitTestCase {
+	/**
+	 * SureCart\Licensing\Client
+	 *
+	 * @var \SureCart\Licensing\Client
+	 */
 	public $client;
 
 	public function setUp() {
@@ -23,5 +28,20 @@ class SettingsTest extends \WP_UnitTestCase {
 			'license key'   => array( 'license_key', 'test_key' ),
 			'activation id' => array( 'activation_id', 'test_activation_id' ),
 		);
+	}
+
+	public function test_get_activation_refreshed_or_not() {
+		// Without refreshed parameter true and activation_id not set, would return false.
+		$this->assertFalse( $this->client->settings()->get_activation() );
+
+		// If passed refreshed = true, then it will try to hard refresh
+		$this->client->settings()->activation_id = 'test-123';
+		$this->assertNotFalse( $this->client->settings()->get_activation( true ) );
+	}
+
+	public function test_get_invalid_activation_wp_error() {
+		$this->client->settings()->activation_id = 'test-123';
+		$result = $this->client->settings()->get_activation( true );
+		$this->assertInstanceOf( 'WP_Error', $result );
 	}
 }
